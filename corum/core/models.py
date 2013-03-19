dd91d95793fd00ac3f7fed88a0b6646f40215805
Charlie Mills <millscr1@cf.ac.uk>
2013-03-19 16:38:49 +0000

    Added user model

--- corum/core/models.py
+++ corum/core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Custom User class, overrides django's User model
+    for additional attributes.
+    """
+
+    def __unicode__(self):
+        return self.username
