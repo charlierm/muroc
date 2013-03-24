c26dad1b4ed8edba172b99246ae7fe957f1710bc
Charlie Mills <millscr1@cf.ac.uk>
2013-03-24 17:25:15 +0000

    Added ViewLog model and view_count property for counting object views.

--- corum/core/models.py
+++ corum/core/models.py
@@ -15,6 +15,16 @@ class AbstractBase(models.Model):
         if not self.id:
             self.id = str(uuid.uuid4())
 
+    @property
+    def view_count(self):
+        """
+        Returns an integer of how many views the current object has.
+        """
+        model_type = ContentType.objects.get_for_model(self)
+        count = ViewLog.objects.filter(content_type__pk=model_type.id,
+                                       object_id=self.id).count()
+        return count
+
     class Meta:
         abstract = True
 
@@ -65,3 +75,30 @@ class Location(AbstractBase):
 
     def __unicode__(self):
         return str(self.latitude) + ", " + str(self.longitude)
+
+
+class ViewLog(AbstractBase):
+    """
+    Used for keeping track of view counts of objects. Uses a generic relation
+    to be used with any model type. This should be implemented in other apps views.
+    """
+    logged_object = generic.GenericForeignKey('content_type', 'object_id')
+    object_id = models.CharField(max_length=36)
+    content_type = models.ForeignKey(ContentType)
+    user = models.ForeignKey(User)
+    time = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def log(cls, model_instance, user):
+        """
+        classmethod for creating a new view log for the current supplied instance.
+
+        @param model_instance The instance of the object to log.
+        @param User The User that viewed the object.
+        @return ViewLog returns the newly created ViewLog instance.
+        """
+        view = cls()
+        view.logged_object = model_instance
+        view.user = user
+        view.save()
+        return view
