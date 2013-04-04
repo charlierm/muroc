57846ec5278f3276d08ed26254937c57051ecb9e
Charlie Mills <millscr1@cf.ac.uk>
2013-04-04 15:40:19 +0100

    Added views for serialising traceroute coords

--- corum/traceroute/views.py
+++ corum/traceroute/views.py
@@ -1,1 +1,26 @@
 # Create your views here.
+import simplekml
+from django.http import HttpResponse
+from traceroute.models import TracerouteResult
+from django.shortcuts import render
+
+class Format:
+    KML = 'kml'
+    GEOJSON = 'json'
+
+
+def traceroute_serialise(request, traceroute_id, format):
+    tr = TracerouteResult.objects.get(pk=traceroute_id)
+    if format == Format.KML:
+        kml = simplekml.Kml()
+        kml.newlinestring(name="Traceroute", description="Traceroute",
+                          coords=tr.line.coords)
+        return HttpResponse(kml.kml())
+
+    elif format == Format.GEOJSON:
+        print tr.line.geojson
+        return HttpResponse(tr.line.geojson)
+
+
+def traceroute_result(request, traceroute_id):
+    return render(request, 'result.html')
