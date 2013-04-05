f63c7711768f260bc35399802032799f160a9c73
Charlie Mills <millscr1@cf.ac.uk>
2013-04-06 00:09:37 +0100

    Added case view at home page

--- corum/core/views.py
+++ corum/core/views.py
@@ -1,7 +1,19 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from core.models import Case
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def home(request):
-    return render(request, 'navbar.html')
+    all_cases = Case.objects.filter(parent_case=None)
+    paginator = Paginator(all_cases, 2)
+    page = request.GET.get('page')
+
+    try:
+        cases = paginator.page(page)
+    except PageNotAnInteger:
+        cases = paginator.page(1)
+    except EmptyPage:
+        cases = paginator.page(paginator.num_pages)
+
+    return render(request, 'cases.html', {'cases': cases})
