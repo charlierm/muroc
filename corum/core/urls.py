174da4e5db2dc641a75506be66646de6e1696834
Charlie Mills <millscr1@cf.ac.uk>
2013-04-07 20:00:40 +0100

    Now using class based views, set default button working.

--- corum/core/urls.py
+++ corum/core/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, url
+from core.views import CaseListView, DefaultCaseView
+
+
+uuid = '[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}'
 
 urlpatterns = patterns('',
-                       url(r'^cases/$', 'core.views.home')
+                       url(r'^cases/$', CaseListView.as_view()),
+                       url(r'^cases/(?P<case_name>.{0,100})/default/$', DefaultCaseView.as_view())
                        )
